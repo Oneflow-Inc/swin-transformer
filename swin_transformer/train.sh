cdb0073a87f56727e5fc8fd005ec3c587464b5e9
rentainhe <596106517@qq.com>
2021-12-10 10:35:57 +0800

    delete useless file

--- swin_transformer/train.sh
+++ swin_transformer/train.sh
@@ -2,4 +2,4 @@ python3 -m oneflow.distributed.launch --nproc_per_node 8 --master_port 12345  ma
                                                                                           --data-path /DATA/disk1/ImageNet/extract/ \
                                                                                           --batch-size 32 \
                                                                                           --tag fix_ddp \
-                                                                                          --resume /DATA/disk1/rentianhe/code/OneFlow-Models/large_scale_training/swin_transformer/output/swin_small_patch4_window7_224/fix_ddp/model_1
+                                                                                          --resume /DATA/disk1/rentianhe/code/OneFlow-Models/large_scale_training/swin_transformer/output/swin_small_patch4_window7_224/fix_ddp/model_2
