e4037da4e3545c2ccf45c95829a1e3c364b85050
BBuf <1182563586@qq.com>
2021-12-13 18:51:40 +0800

    print time

--- swin_transformer/debug.sh
+++ swin_transformer/debug.sh
@@ -5,7 +5,7 @@
 #                     --tag fix_ddp \
 
 python3 debug.py --cfg configs/swin_small_patch4_window7_224.yaml \
-        --batch-size 2
+        --batch-size 32
 
 # python3 -m oneflow.distributed.launch --nproc_per_node 1 --master_port 12345  \
 #         debug.py --cfg configs/swin_small_patch4_window7_224.yaml \
