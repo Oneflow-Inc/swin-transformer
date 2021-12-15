20988ab0dea8bffd286d10949704bf801b3a9946
lyon <flowingsun007@163.com>
2021-12-15 18:14:36 +0800

    profile

--- swin_transformer/debug_with_real_data.py
+++ swin_transformer/debug_with_real_data.py
@@ -62,7 +62,7 @@ def parse_option():
 
     return args, config
 
-if __name__ == '__main__':
+def run():
     args, config = parse_option()
 
     # if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
@@ -135,6 +135,29 @@ if __name__ == '__main__':
     total_time = time.time() - start
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print(total_time_str)
+
+
+if __name__ == '__main__':
+    # run without profile >>> bash debug_with_real_data.sh
+    run()
+
+    # run with line_profiler profile >>> bash debug_with_real_data.sh > line_profile_torch.log 2>&1
+    # from line_profiler import LineProfiler
+    # lp = LineProfiler()
+    # lp_wrapper = lp(run)
+    # lp_wrapper()
+    # lp.print_stats()
+
+    # run with cProfile profile >>> bash debug_with_real_data.sh > cProfile_torch.log 2>&1
+    # import cProfile, pstats
+    # cp = cProfile.Profile()
+    # cp.enable()
+    # run()
+    # cp.disable()
+    # stats = pstats.Stats(cp).sort_stats('cumtime')
+    # stats.print_stats()
+    
+
 
 
 
