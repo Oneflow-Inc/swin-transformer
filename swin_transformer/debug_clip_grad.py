dcfb5cd1f5095b0e1517d1545c63083ee86b5344
lyon <flowingsun007@163.com>
2021-12-16 11:33:36 +0800

    profile_test3

--- swin_transformer/debug_clip_grad.py
+++ swin_transformer/debug_clip_grad.py
@@ -1,153 +1,41 @@
-# --------------------------------------------------------
-# Swin Transformer
-# Copyright (c) 2021 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ze Liu
-# --------------------------------------------------------
-
-import os
 import time
-import argparse
-import datetime
-
 import numpy as np
-
 import oneflow as flow
-
-# import oneflow.backends.cudnn as cudnn
-
-from flowvision.loss.cross_entropy import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
-from flowvision.utils.metrics import accuracy, AverageMeter
-
-from config import get_config
-from models import build_model
-from data import build_loader
-# from lr_scheduler import build_scheduler
-from optimizer import build_optimizer
-# from logger import create_logger
-# from utils import load_checkpoint, save_checkpoint, get_grad_norm, auto_resume_helper, reduce_tensor
-
-
-def parse_option():
-    parser = argparse.ArgumentParser('Swin Transformer training and evaluation script', add_help=False)
-    parser.add_argument('--cfg', type=str, required=True, metavar="FILE", help='path to config file', )
-    parser.add_argument(
-        "--opts",
-        help="Modify config options by adding 'KEY VALUE' pairs. ",
-        default=None,
-        nargs='+',
-    )
-
-    # easy config modification
-    parser.add_argument('--batch-size', type=int, help="batch size for single GPU")
-    parser.add_argument('--data-path', type=str, help='path to dataset')
-    parser.add_argument('--zip', action='store_true', help='use zipped dataset instead of folder dataset')
-    parser.add_argument('--cache-mode', type=str, default='part', choices=['no', 'full', 'part'],
-                        help='no: no cache, '
-                             'full: cache all data, '
-                             'part: sharding the dataset into nonoverlapping pieces and only cache one piece')
-    parser.add_argument('--resume', help='resume from checkpoint')
-    parser.add_argument('--accumulation-steps', type=int, help="gradient accumulation steps")
-    parser.add_argument('--use-checkpoint', action='store_true',
-                        help="whether to use gradient checkpointing to save memory")
-    parser.add_argument('--output', default='output', type=str, metavar='PATH',
-                        help='root of output folder, the full path is <output>/<model_name>/<tag> (default: output)')
-    parser.add_argument('--tag', help='tag of experiment')
-    parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
-    parser.add_argument('--throughput', action='store_true', help='Test throughput only')
-
-    args, unparsed = parser.parse_known_args()
-
-    config = get_config(args)
-
-    return args, config
+# import torch as flow
 
 def run():
-    args, config = parse_option()
-    model = build_model(config)
-    model.cuda()
-    optimizer = build_optimizer(config, model)
+    arr = np.random.randn(128, 30, 224, 224)    
+    input = flow.tensor(arr, dtype=flow.float32, device="cuda", requires_grad=True)
+    start = time.time()
+    for idx in range(1000):
+        m = flow.nn.ReLU()
+        out = m(input)
+        out = out.sum()
+        out.backward()
+        grad_norm = flow.nn.utils.clip_grad_norm_(input, 5.0)
+        print("iter >>>>>>>> ", idx+1, " grad_norm >>>>>>> ", grad_norm.cpu().numpy().sum())
+    print("cost: ", time.time()-start)
 
-    dataset_train, dataset_val, data_loader_train, data_loader_val, mixup_fn = build_loader(config)
-
-    if config.AUG.MIXUP > 0.:
-        # smoothing is handled with mixup label transform
-        criterion = SoftTargetCrossEntropy()
-    elif config.MODEL.LABEL_SMOOTHING > 0.:
-        criterion = LabelSmoothingCrossEntropy(smoothing=config.MODEL.LABEL_SMOOTHING)
-    else:
-        criterion = flow.nn.CrossEntropyLoss()
-
-    optimizer = build_optimizer(config, model)
-    # model = flow.nn.parallel.DistributedDataParallel(model, broadcast_buffers=False)
-    # model_without_ddp = model
-
-    # criterion = flow.nn.CrossEntropyLoss()
-    # data_loader_train_iter = iter(data_loader_train)
-
-    batch_time = AverageMeter()
-    loss_meter = AverageMeter()
-    norm_meter = AverageMeter()
-
-    max_accuracy = 0.0
-    start_time = time.time()
-    end = time.time()
-
-    # samples, targets = data_loader_train_iter.__next__()
-    # samples = samples.cuda()
-    # targets = targets.cuda()
-    samples = flow.ones(config.DATA.BATCH_SIZE, 3, 224, 224, dtype=flow.float32, device="cuda")
-    targets = flow.ones(config.DATA.BATCH_SIZE, 3, dtype=flow.int64, device="cuda")
-
-    for idx in range(200):
-        model.train()
-        optimizer.zero_grad()
-
-        # samples, targets = data_loader_train_iter.__next__()
-        # samples = samples.cuda()
-        # targets = targets.cuda()
-
-        if mixup_fn is not None:
-            samples, targets = mixup_fn(samples, targets)
-        
-        outputs = model(samples)
-        outputs.sum().backward()
-
-        if config.TRAIN.CLIP_GRAD:
-            grad_norm = flow.nn.utils.clip_grad_norm_(model.parameters(), config.TRAIN.CLIP_GRAD)
-        optimizer.step()
-
-        # loss_meter.update(loss.item(), targets.size(0))
-        if config.TRAIN.CLIP_GRAD:
-            norm_meter.update(grad_norm)
-        batch_time.update(time.time() - end)
-        end = time.time()
-        print("step >>>>>>>> ", idx, "finish!")
-
-    print(outputs)
-    total_time = time.time() - start_time
-    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print(total_time_str)
 
 if __name__ == '__main__':
     # run without profile >>> bash debug_with_real_data.sh
-    run()
+    # run()
 
-    # run with line_profiler profile >>> bash debug_with_real_data.sh > line_profile_flow.log 2>&1
+    # run with line_profiler profile >>> bash debug_clip_grad.sh > line_profile_clipgrad_flow.log 2>&1
     # from line_profiler import LineProfiler
     # lp = LineProfiler()
     # lp_wrapper = lp(run)
     # lp_wrapper()
     # lp.print_stats()
 
-    # run with cProfile profile >>> bash debug_with_real_data.sh > cProfile_flow.log 2>&1
-    # import cProfile, pstats
-    # cp = cProfile.Profile()
-    # cp.enable()
-    # run()
-    # cp.disable()
-    # stats = pstats.Stats(cp).sort_stats('cumtime')
-    # stats.print_stats()
+    # run with cProfile profile >>> bash debug_clip_grad.sh > cProfile_clipgrad_flow.log 2>&1
+    import cProfile, pstats
+    cp = cProfile.Profile()
+    cp.enable()
+    run()
+    cp.disable()
+    stats = pstats.Stats(cp).sort_stats('cumtime')
+    stats.print_stats()
 
 
 
