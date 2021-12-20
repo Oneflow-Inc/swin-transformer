9fb18ab926bb87ab686ea7467620b85d3285c383
Ldpe2G <liangdepeng@gmail.com>
2021-12-20 12:40:09 +0800

    add vit graph use fake data script

---
+++ swin_transformer/main_vit_consistent_graph_use_fake_data.py
@@ -0,0 +1,174 @@
+import os
+import time
+import argparse
+import datetime
+import numpy as np
+import oneflow as flow
+import oneflow.backends.cudnn as cudnn
+
+# from cross_entropy import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
+from flowvision.loss.cross_entropy import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
+from flowvision.utils.metrics import accuracy, AverageMeter
+
+from config import get_config
+from models import build_vit
+from data import build_loader
+from lr_scheduler import build_scheduler
+from optimizer import build_optimizer
+from logger import create_logger
+from utils import load_checkpoint, save_checkpoint, get_grad_norm, auto_resume_helper, reduce_tensor
+from models.graph import TrainGraph, EvalGraph
+
+
+def parse_option():
+    parser = argparse.ArgumentParser('Swin Transformer training and evaluation script', add_help=False)
+    parser.add_argument('--cfg', type=str, required=True, metavar="FILE", help='path to config file', )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options by adding 'KEY VALUE' pairs. ",
+        default=None,
+        nargs='+',
+    )
+
+    # easy config modification
+    parser.add_argument('--batch-size', type=int, help="batch size for single GPU")
+    parser.add_argument('--data-path', type=str, help='path to dataset')
+    parser.add_argument('--zip', action='store_true', help='use zipped dataset instead of folder dataset')
+    parser.add_argument('--cache-mode', type=str, default='part', choices=['no', 'full', 'part'],
+                        help='no: no cache, '
+                             'full: cache all data, '
+                             'part: sharding the dataset into nonoverlapping pieces and only cache one piece')
+    parser.add_argument('--resume', help='resume from checkpoint')
+    parser.add_argument('--accumulation-steps', type=int, help="gradient accumulation steps")
+    parser.add_argument('--use-checkpoint', action='store_true',
+                        help="whether to use gradient checkpointing to save memory")
+    parser.add_argument('--output', default='output', type=str, metavar='PATH',
+                        help='root of output folder, the full path is <output>/<model_name>/<tag> (default: output)')
+    parser.add_argument('--tag', help='tag of experiment')
+    parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
+    parser.add_argument('--throughput', action='store_true', help='Test throughput only')
+    parser.add_argument("--model_arch", type=str, default="vit_b_16_224",
+                        choices=["vit_b_16_224",
+                                 "vit_b_16_384",
+                                 "vit_b_32_224",
+                                 "vit_b_32_384",
+                                 "vit_l_16_224",
+                                 "vit_l_16_384"], help="model architecture", )
+    parser.add_argument('--is-consistent', action='store_true', help='model type')
+
+    args, unparsed = parser.parse_known_args()
+
+    config = get_config(args)
+
+    return args, config
+
+
+def main(config):
+    logger.info(f"Creating model:{config.MODEL.TYPE}/{config.MODEL.NAME}")
+    model = build_vit.build_model(config)
+    model.cuda()
+    logger.info(str(model))
+
+    model = model.to_consistent(
+        placement=flow.placement("cuda", {0: range(flow.env.get_world_size())}), sbp=flow.sbp.broadcast)
+    optimizer = build_optimizer(config, model)
+
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    logger.info(f"number of params: {n_parameters}")
+    lr_scheduler = flow.optim.lr_scheduler.CosineAnnealingLR(optimizer, 2)
+    criterion = flow.nn.CrossEntropyLoss()
+
+    logger.info("Start training")
+    start_time = time.time()
+    train_graph = TrainGraph(model=model,
+                             loss_fn=criterion,
+                             optimizer=optimizer,
+                             lr_scheduler=lr_scheduler,
+                             accumulation_steps=config.TRAIN.ACCUMULATION_STEPS)
+    for epoch in range(config.TRAIN.START_EPOCH, config.TRAIN.EPOCHS):
+        train_one_epoch(config, model, optimizer, epoch, lr_scheduler, train_graph)
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    logger.info('Training time {}'.format(total_time_str))
+
+
+def train_one_epoch(config, model, optimizer, epoch, lr_scheduler, train_graph, mixup_fn = None):
+    model.train()
+
+    num_steps = 1000
+    batch_time = AverageMeter()
+    loss_meter = AverageMeter()
+    norm_meter = AverageMeter()
+
+    image = flow.ones(config.DATA.BATCH_SIZE * flow.env.get_world_size(), 3, 224, 224, placement=flow.placement("cuda", {0: range(flow.env.get_world_size())}), sbp=flow.sbp.split(0))
+    targets = flow.ones(config.DATA.BATCH_SIZE * flow.env.get_world_size(), placement=flow.placement("cuda", {0: range(flow.env.get_world_size())}), sbp=flow.sbp.split(0), dtype=flow.int32)
+
+    start = time.time()
+    end = time.time()
+    for idx in range(num_steps):
+
+        if mixup_fn is not None:
+            image, targets = mixup_fn(image, targets)
+
+        loss = train_graph(image, targets).to_local()
+
+        loss_meter.update(loss.item(), targets.size(0))
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if idx % config.PRINT_FREQ == 0:
+            lr = optimizer.param_groups[0]['lr']
+            etas = batch_time.avg * (num_steps - idx)
+            logger.info(
+                f'Train: [{epoch}/{config.TRAIN.EPOCHS}][{idx}/{num_steps}]\t'
+                f'eta {datetime.timedelta(seconds=int(etas))} lr {lr:.6f}\t'
+                f'time {batch_time.val:.4f} ({batch_time.avg:.4f})\t'
+                f'loss {loss_meter.val:.4f} ({loss_meter.avg:.4f})\t'
+                f'grad_norm {norm_meter.val:.4f} ({norm_meter.avg:.4f})\t')
+    epoch_time = time.time() - start
+    logger.info(f"EPOCH {epoch} training takes {datetime.timedelta(seconds=int(epoch_time))}")
+
+if __name__ == '__main__':
+    _, config = parse_option()
+
+    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        rank = flow.env.get_rank()
+        world_size = flow.env.get_world_size()
+        print(f"RANK and WORLD_SIZE in environ: {rank}/{world_size}")
+    else:
+        rank = -1
+        world_size = -1
+
+    seed = config.SEED + flow.env.get_rank()
+    flow.manual_seed(seed)
+    np.random.seed(seed)
+    cudnn.benchmark = True
+
+    linear_scaled_lr = config.TRAIN.BASE_LR * config.DATA.BATCH_SIZE * flow.env.get_world_size() / 512.0
+    linear_scaled_warmup_lr = config.TRAIN.WARMUP_LR * config.DATA.BATCH_SIZE * flow.env.get_world_size() / 512.0
+    linear_scaled_min_lr = config.TRAIN.MIN_LR * config.DATA.BATCH_SIZE * flow.env.get_world_size() / 512.0
+    # gradient accumulation also need to scale the learning rate
+    if config.TRAIN.ACCUMULATION_STEPS > 1:
+        linear_scaled_lr = linear_scaled_lr * config.TRAIN.ACCUMULATION_STEPS
+        linear_scaled_warmup_lr = linear_scaled_warmup_lr * config.TRAIN.ACCUMULATION_STEPS
+        linear_scaled_min_lr = linear_scaled_min_lr * config.TRAIN.ACCUMULATION_STEPS
+    config.defrost()
+    config.TRAIN.BASE_LR = linear_scaled_lr
+    config.TRAIN.WARMUP_LR = linear_scaled_warmup_lr
+    config.TRAIN.MIN_LR = linear_scaled_min_lr
+    config.freeze()
+
+    os.makedirs(config.OUTPUT, exist_ok=True)
+    logger = create_logger(output_dir=config.OUTPUT, dist_rank=flow.env.get_rank(), name=f"{config.MODEL.NAME}")
+
+    if flow.env.get_rank() == 0:
+        path = os.path.join(config.OUTPUT, "config.json")
+        with open(path, "w") as f:
+            f.write(config.dump())
+        logger.info(f"Full config saved to {path}")
+
+    # print config
+    logger.info(config.dump())
+
+    main(config)
---
+++ swin_transformer/main_vit_consistent_graph_use_fake_data.sh
@@ -0,0 +1,3 @@
+ python3 -m oneflow.distributed.launch \
+    --nproc_per_node 8 --master_port 12345 --node_rank 0 --nnodes 1 \
+    main_vit_consistent_graph_use_fake_data.py --cfg configs/vit_base_patch16_224.yaml --batch-size 2
