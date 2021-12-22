24a9d5f24911182e54da03d863a26313c87e0c8b
Ldpe2G <liangdepeng@gmail.com>
2021-12-22 14:27:22 +0800

    add loss compare scripts for other models

--- compare_v2.sh
+++ compare_v2.sh
@@ -1,11 +1,11 @@
 set -aux
 
 DATA_PATH="/DATA/disk1/ImageNet/extract/"
-BATCH_SIZE=16
-TOTAL_ITERS=100
+BATCH_SIZE=8
+TOTAL_ITERS=500
 
 
-python loss_compare2.py --batch_size $BATCH_SIZE \
+python3 loss_compare_other_nets.py --batch_size $BATCH_SIZE \
                        --data_path $DATA_PATH \
                        --total_iters $TOTAL_ITERS \
                        --draw
