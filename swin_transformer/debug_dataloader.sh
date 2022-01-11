702c71eb7821d88827c2803607c02f06f05bc772
BBuf <1182563586@qq.com>
2022-01-11 09:10:09 +0000

    add oneflow dataloader script

---
+++ swin_transformer/debug_dataloader.sh
@@ -0,0 +1,5 @@
+
+python3  -m torch.distributed.launch --nproc_per_node 1 --master_port 12345 \
+        debug_dataloader.py --cfg configs/swin_tiny_patch4_window7_224.yaml \
+        --batch-size 32 \
+        --data-path /dataset/extract/ \
