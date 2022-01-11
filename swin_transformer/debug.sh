fa1100fbc22fa95600a29ff76edbe09d0d37d25c
BBuf <1182563586@qq.com>
2022-01-11 02:56:31 +0000

    default imagenet

--- swin_transformer/debug.sh
+++ swin_transformer/debug.sh
@@ -4,7 +4,7 @@
 #                     --batch-size 32 \
 #                     --tag fix_ddp \
 
-python3 debug.py --cfg configs/swin_small_patch4_window7_224.yaml \
+python3 debug.py --cfg configs/swin_tiny_patch4_window7_224.yaml \
         --batch-size 32
 
 # python3 -m oneflow.distributed.launch --nproc_per_node 1 --master_port 12345  \
